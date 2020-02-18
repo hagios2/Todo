578030208b15cc9f6ede689110c02944458eb616
hagios <hagioswilson@gmail.com>
2020-02-18 17:31:39 +0000

    initial setup

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'database' => [
+
+        'dbname' => 'todosss',
+
+        'username' => 'admin',
+
+        'password' => 'iamadmin8853',
+
+        'connection' => 'mysql:host=127.0.0.1',
+
+        'options' => []
+
+    ]
+
+
+];
---
+++ database/Connection.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Connection 
+{
+    
+    public static function make($config)
+    {
+
+        try {
+
+            return new PDO(
+
+                $config['connection'].':dbname='.$config['dbname'], 
+                
+                $config['username'],
+                
+                $config['password'],
+
+                $config['options']
+            );
+        
+       
+        } catch(PDOException $e) {
+
+            die($e->getMessage());
+        }
+    }
+}
